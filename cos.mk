380119070ea86b7eca328e37cd06f2461483e435
ZerNico <ZerNico.yt@gmail.com>
2017-04-09 19:31:36 +0200

    Cosmic-ify

--- cos.mk
+++ cos.mk
@@ -19,13 +19,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/onyx/device.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/validus/config/common_full_phone.mk)
-$(call inherit-product, vendor/validus/config/caf_fw.mk)
+$(call inherit-product, vendor/cos/common.mk)
 
 # Call the proprietary setup
 $(call inherit-product-if-exists, vendor/oneplus/onyx/onyx-vendor.mk)
 
-PRODUCT_NAME := validus_onyx
+PRODUCT_NAME := cos_onyx
 PRODUCT_DEVICE := onyx
 PRODUCT_MANUFACTURER := OnePlus
 
@@ -35,5 +34,5 @@ PRODUCT_BRAND := OnePlus
 TARGET_VENDOR := oneplus
 TARGET_VENDOR_PRODUCT_NAME := onyx
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-	DEVICE_MAINTAINERS="Nimit Mehta (CheckYourScreen)"
+COS_RELEASE=true
+USE_LAUNCHER3=true
